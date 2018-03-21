61db50e1dc40e645f4e06f7d5ce8c80b5948c622
jkdlt22 <37200940+jkdlt22@users.noreply.github.com>
2018-03-21 11:39:53 +1100

    data added
    dummy data added into the views.py

--- siteroot/mysite/views.py
+++ siteroot/mysite/views.py
@@ -10,3 +10,37 @@ def index(request):
 		'username':'James',
 	}
 	return render(request, 'index.html', context)
+
+#function based views with dummy data
+def feedback(request):
+
+	#Dummy Data
+	context ={	
+		'email':'abc@gmail.com',
+		'full_name':'Jon DT',
+		'title':'title',
+		'message':'message',
+	}
+	return render(request, 'feedback.html', context)
+
+def login(request):
+	
+	#Dummy Data
+	context ={
+		'username':'James123',
+		'password':'abc123',
+	}
+	return render(request, 'login.html', context)
+
+def profile(request):
+	
+	#Dummy Data
+	context ={
+		'username':'James123',
+		'first_name':'James',
+		'last_name':'McDowell',
+		'email':'abc@gmail.com',
+		'date_birth':'14/04/1995',
+		'sessions_played':'5',
+	}
+	return render(request, 'profile.html', context)
