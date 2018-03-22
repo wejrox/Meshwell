da6c738088ea5631b86936485f603736fcecf8f5
jkdlt22 <37200940+jkdlt22@users.noreply.github.com>
2018-03-22 12:48:26 +1100

    Update forms.py

--- siteroot/mysite/forms.py
+++ siteroot/mysite/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-  full_name = forms.CharField()
+  full_name = forms.CharField(max_length=100)
   email = forms.emailField()
-  title = forms.CharField()
-  message = forms.CharField()
+  title = forms.CharField(max_length=100)
+  message = forms.CharField(widget=forms.Textarea)
