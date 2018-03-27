10f663f07ad31c2e06748296c41fb3756a8a255c
jkdlt22 <37200940+jkdlt22@users.noreply.github.com>
2018-03-27 20:15:47 +1100

    added urls for django admin

--- siteroot/mysite/urls.py
+++ siteroot/mysite/urls.py
@@ -19,6 +19,7 @@ from mysite import views as site_views
 from apps.api.urls import router as api_router
 from rest_framework.authtoken import views as token_views
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
@@ -31,6 +32,9 @@ urlpatterns = [
     path('account/deactivate/', site_views.deactivate_user, name='deactivate'),
     path('account/login/', auth_views.LoginView.as_view(), name='login'),
     path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(‘admin/’, admin.site.urls),
+    path('', views.posts, name='posts'),
+    path('', views.comments, name='comments'),
     # API
     url(r'^api/', include(api_router.urls)),
 ]
