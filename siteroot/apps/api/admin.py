b861d033919f9861abbb746d35334dcbd1b39b5a
jkdlt22 <s3487373@student.rmit.edu.au>
2018-03-29 10:52:14 +1100

    Changes to be committed: 	modified:   apps/api/admin.py

--- siteroot/apps/api/admin.py
+++ siteroot/apps/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from ..api.models import Profile, Availability, Game, Game_Role, Session, Session_Profile, Report, Profile_Connected_Game_Account, Game_Api_Connection, Feedback
+
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Availability)
+admin.site.register(Game)
+admin.site.register(Game_Role)
+admin.site.register(Session)
+admin.site.register(Session_Profile)
+admin.site.register(Report)
+admin.site.register(Profile_Connected_Game_Account)
+admin.site.register(Game_Api_Connection)
+admin.site.register(Feedback)
