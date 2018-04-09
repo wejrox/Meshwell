615bebf8f582a6b13cdd5bd05e36538bbb5e2ffe
mihir <mihir1914@gmail.com>
2018-04-10 09:08:44 +1000

    added some changes to admin.py

--- siteroot/apps/api/admin.py
+++ siteroot/apps/api/admin.py
@@ -2,36 +2,14 @@ from django.contrib import admin
 from ..api.models import Profile, Availability, Game, Game_Role, Session, Session_Profile, Report, Profile_Connected_Game_Account, Game_Api_Connection, Feedback
 
 # Register your models here.
-#admin.site.register(Profile)
 admin.site.register(Availability)
 admin.site.register(Game)
 admin.site.register(Game_Role)
 admin.site.register(Session)
 admin.site.register(Session_Profile)
-#admin.site.register(Report)
 admin.site.register(Profile_Connected_Game_Account)
 admin.site.register(Game_Api_Connection)
 admin.site.register(Feedback)
-
-
-
-
-#class ReportAdmin(admin.ModelAdmin)
-	#list_display = ('report_reason')
-#admin.site.register(Report, ReportAdmin)
-
-#BanUser(modeladmin, request, queryset):
-	#for report in queryset:
-
-		#user.is_active == false
-		#profile.save()
-
-#class Profile(admin.ModelAdmin):
-#	list_display = ('id', 'birth_date', 'sessions_played')
-#	actions = [BanUser, ]
-
-	#admin.site.register(Profile, ProfileAdmin)
-
 
 class ReportAdmin(admin.ModelAdmin):
 	pass
@@ -57,14 +35,3 @@ class ProfileAdmin(admin.ModelAdmin):
         return total
 
 admin.site.register(Profile, ProfileAdmin)
-
-
-
-
-
-
-#class ReportAdmin(admin.ModelAdmin):
-#    pass
-
-
-#admin.site.register(Report,ReportAdmin)
