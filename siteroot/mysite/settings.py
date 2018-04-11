df9f751c9d298a367aaf815017ef7d3cfd5d1d5d
ashneelDeo <s3545429@student.rmit.edu.au>
2018-04-11 18:08:45 +1000

    Fixed incorrect settings.py reference

--- siteroot/mysite/settings.py
+++ siteroot/mysite/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'mysite.context_processors.login_form',
+                'mysite.context_processors.login_modal',
             ],
         },
     },
