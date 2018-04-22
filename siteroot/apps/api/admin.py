3a86ea92454ca94ecce5b61814ee8769923533fd
root <mihir1914@gmail.com>
2018-04-22 21:21:43 +1000

    Function to allow users to be banned through a button is created

--- siteroot/apps/api/admin.py
+++ siteroot/apps/api/admin.py
@@ -3,6 +3,7 @@ from ..api.models import Profile, Availability, Game, Game_Role, Session, Sessio
 from django.db.models import Count
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.contrib import messages
 #from django.contrib.auth.models import User
 #from django.contrib.auth.models import User
 
@@ -20,33 +21,27 @@ class ReportAdmin(admin.ModelAdmin):
 admin.site.register(Report, ReportAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
-	list_display = ('user', 'birth_date', 'sessions_played')
+	list_display = ('user', 'birth_date', 'sessions_played', 'total_reports')
 	readonly_fields = (('sessions_played'),('birth_date'),('user'),('pref_server'),('teamwork_commends'),('skill_commends'),('positivity_commends'),('communication_commends'),('discord_name'))#,'total_reports')
 
-#USER BANNING FUNCTION, CURRENTLY UNDER DEVELOPMENT
-	#readonly_fields = (('user'), ('birth_date'), ('sessions_played') , ('commendations'))
-	#actions = ['ban_users']
-	#def total_reports(self, obj):
-		#return Report.objects.filter(user_reported=obj).count()
-
-	#def ban_users(self, request, queryset):
-		#self.user.objects.update(is_active = False)
-		#self.message_user(request, "Banned and Sent Email")
-		#User.save()
-	#ban_users.short_description = "Ban & Email"
-
+	def total_reports(self, obj):
+		return Report.objects.filter(user_reported=obj).count()
 
 admin.site.register(Profile, ProfileAdmin)
 
 class MyUserAdmin(UserAdmin):
 	list_display = ('username', 'first_name', 'last_name' , 'email')
 	readonly_fields = ('first_name' , ('last_name') , ('email') , ('username'))
+	actions = ['ban_users', 'remove_ban']
 
+	#USER BANNING FUNCTION, CURRENTLY UNDER DEVELOPMENT, Email stub is also here
+	#Banning and emailing function can be merged into one so emails are sent when users are banned
+	def ban_users(self, request, queryset):
+		queryset.update(is_active = False)
+		self.message_user(request, "User is banned and Email has been sent")
+
+	def remove_ban(self, request, queryset):
+		queryset.update(is_active = True)
+		self.message_user(request, "Users ban has been liften")
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
-
-#USER BANNING FUNCTION, CURRENTLY UNDER DEVELOPMENT
-#def banning(self, request, queryset):
-#	self.user.objects.all().update(is_active=false)
-	#self.message_user(request, "BANNED")
-	#user.save()
