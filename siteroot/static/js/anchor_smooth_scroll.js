7527b696f5ede9989b43f6f5d8cf0d58ac27b02c
ashneelDeo <s3545429@student.rmit.edu.au>
2018-04-30 20:46:11 +1000

    Implementing new homepage
    New design test for homepage.

---
+++ siteroot/static/js/anchor_smooth_scroll.js
@@ -0,0 +1,7 @@
+//Anchor link smooth scroller
+$(function() {
+			  $('a[href*=#]').on('click', function(e) {
+			    e.preventDefault();
+			    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+			  });
+			});
---
+++ siteroot/static/js/bootstrap_tooltip.js
@@ -0,0 +1,4 @@
+// Bootstrap tooltip support
+$(function () {
+			  $('[data-toggle="tooltip"]').tooltip();
+			})
