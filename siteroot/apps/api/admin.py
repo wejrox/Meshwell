f3e0432371fc7ffa512e5e657fbe5d44c5ef8083
mihir <mihir1914@gmail.com>
2018-05-10 09:45:01 +1000

    added emailing function in admin

--- siteroot/apps/api/admin.py
+++ siteroot/apps/api/admin.py
@@ -4,6 +4,8 @@ from django.db.models import Count
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 
@@ -17,36 +19,39 @@ admin.site.register(Profile_Connected_Game_Account)
 admin.site.register(Feedback)
 
 
-#USER BANNING FUNCTION, CURRENTLY UNDER DEVELOPMENT, Email stub is also here
-#Banning and emailing function can be merged into one so emails are sent when users are banned
-#def ban_users(self, request, queryset):
-#	queryset.update(is_active = False)
-#	self.message_user(request, "User is banned and Email has been sent")
-
-
+#User Banning and emailing Function
 def banning_users(self, request, queryset):
-    for obj in queryset:
-        if hasattr(obj, 'user'):
-            # This object is a Profile, so lookup the user
-            obj = obj.user
-        obj.is_active = False
-        obj.save()
-    self.message_user(request, "User is banned and Email has been sent")
+	for obj in queryset:
+		if hasattr(obj, 'user'):
+			# This object is a Profile, so lookup the user
+			obj = obj.user
+		obj.is_active = False
+		#Sends ban email to user,does not send through the variables yet
+		subject = 'Ban'
+		message = ' You have been banned for being trash '
+		email_from = settings.EMAIL_HOST_USER
+		recipient_list = [obj.email]
+		send_mail( subject, message,email_from, recipient_list )
+		obj.save()
+	self.message_user(request, "User is banned and Email has been sent")
 
 def unbanning_users(self, request, queryset):
-    for obj in queryset:
-        if hasattr(obj, 'user'):
-            # This object is a Profile, so lookup the user
-            obj = obj.user
-        obj.is_active = True
-        obj.save()
-    self.message_user(request, "User is unbanned")
+	for obj in queryset:
+		if hasattr(obj, 'user'):
+			# This object is a Profile, so lookup the user
+			obj = obj.user
+		obj.is_active = True
+		obj.save()
+	self.message_user(request, "User is unbanned")
 
+#Ban only + add to database function
 def ban_users(self, request, queryset):
 	queryset.update(is_active = False)
 	banned_user = Banned_User.objects.create(profile=request.user.profile)
 	banned_user.save()
 	self.message_user(request, "user banned")
+
+
 
 def remove_ban(self, request, queryset):
 	print(queryset)
@@ -55,7 +60,6 @@ def remove_ban(self, request, queryset):
 	#banned_user.save()
 	self.message_user(request, "Users ban has been lifted")
 
-#Admin functions will be created here,as well as registration  of their specific models
 class ReportAdmin(admin.ModelAdmin):
 	list_display = ('user_reported', 'report_reason', 'sent_by', 'session')
 
@@ -66,11 +70,9 @@ class ProfileAdmin(admin.ModelAdmin):
 
 	list_display = ('user', 'birth_date', 'sessions_played', 'total_reports', 'reason_reported_sent')
 	readonly_fields = (('sessions_played'),('birth_date'),('user'),('pref_server'),('teamwork_commends'),('skill_commends'),('sportsmanship_commends'),('communication_commends'),('discord_id'))#,'total_reports')
-	actions = ['ban1', 'unban1']
+	actions = ['ban1', 'unban1','email_ban']
 	ban1 = banning_users
 	unban1 = unbanning_users
-
-
 
 	def total_reports(self, obj):
 		return Report.objects.filter(user_reported=obj).count()
@@ -83,7 +85,8 @@ admin.site.register(Profile, ProfileAdmin)
 class MyUserAdmin(UserAdmin):
 	list_display = ('userprofile','username', 'first_name', 'last_name' , 'email')
 	readonly_fields = ('first_name' , ('last_name') , ('email') , ('username'))
-	actions = [ban_users, remove_ban]
+	actions = [banning_users, remove_ban]
+
 	#ban = ban_users
 	#unban = remove_ban
 
