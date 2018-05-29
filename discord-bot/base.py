5e2f6e9b5a6b8bf4aa3d9437aa612db44c6ffe12
wejrox <james.mcdowell001@gmail.com>
2018-05-29 11:58:34 +0000

    Fixed discord bot, wasn't casting id to integer

--- discord-bot/base.py
+++ discord-bot/base.py
@@ -11,7 +11,7 @@ import sys
 # Run code on exit of script
 import atexit
 
-debug = False
+debug = True
 bot = commands.Bot(command_prefix=bot_settings.cmd_prefix)
 
 # Database
@@ -52,12 +52,17 @@ async def auto_manage_channels():
 	# Query database every x timeframe
 	print("=================\nRunning\n=================")
 	while not bot.is_closed():
+		print("Checking for sessions")
 		# Get upcoming sessions
 		cursor = db_connection.cursor()
+		print("Made cursor")
 		cursor.execute(query_data.upcoming_sessions_query)
+		print("Executed query")
 		upcoming = cursor.fetchall()
+		print("About to check them")
 		# Create channels if needed
 		if len(upcoming) > 0:
+			print('Found Something')
 			# Run only if in debug mode for performance reasons
 			if debug:
 				print("Fetched upcoming session details [session_id, discord_id]:")
@@ -77,7 +82,7 @@ async def auto_manage_channels():
 					# Set default perms
 					await channel.set_permissions(guild.default_role, connect=False)
 					# Assign permissions to channel for users
-					member = guild.get_member(row[1])
+					member = guild.get_member(int(row[1]))
 					if member is not None:
 						await channel.set_permissions(member, connect=True)
 						print("Permissions set")
--- discord-bot/query_data.py
+++ discord-bot/query_data.py
@@ -1,8 +1,8 @@
-query_interval = 300 # Amount of time in between each query to the database
+query_interval = 300 # Amount of time in between each query to the database in seconds
 
 # Get any sessions that haven't happened yet but will happen in the next 10 minutes
 upcoming_sessions_query = (
-    "SELECT api_session.id as session_id, api_profile.discord_name"
+    "SELECT api_session.id as session_id, api_profile.discord_id"
     " FROM api_session, api_profile, api_session_profile"
     " WHERE api_session_profile.session_id = api_session.id"
     " AND api_session_profile.profile_id = api_profile.id"
@@ -14,7 +14,7 @@ upcoming_sessions_query = (
 
 # Get any sessions that ended since the last interval
 past_sessions_query = (
-    "SELECT api_session.id as session_id, api_profile.discord_name, api_session_profile.id"
+    "SELECT api_session.id as session_id, api_profile.discord_id, api_session_profile.id"
     " FROM api_session, api_profile, api_session_profile"
     " WHERE api_session_profile.session_id = api_session.id"
     " AND api_session_profile.profile_id = api_profile.id"
